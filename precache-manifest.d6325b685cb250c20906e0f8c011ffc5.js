71e2a76269294c6b3c1397b1631720487c6ab9ed
frknckc <furkancakici25@gmail.com>
2020-03-23 17:56:28 +0300

    Updates

---
+++ precache-manifest.d6325b685cb250c20906e0f8c011ffc5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b08bdddefb7f9e74a61d8cfae93cd9e4",
+    "url": "/reading-list-app-reactjs/index.html"
+  },
+  {
+    "revision": "f03c02709449d0e55357",
+    "url": "/reading-list-app-reactjs/static/css/main.defcf392.chunk.css"
+  },
+  {
+    "revision": "77da99498ce6e7c6fcee",
+    "url": "/reading-list-app-reactjs/static/js/2.a7b6decf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reading-list-app-reactjs/static/js/2.a7b6decf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f03c02709449d0e55357",
+    "url": "/reading-list-app-reactjs/static/js/main.daf040e3.chunk.js"
+  },
+  {
+    "revision": "df3e7bff0207b2d84d98",
+    "url": "/reading-list-app-reactjs/static/js/runtime-main.e77ab56c.js"
+  }
+]);
